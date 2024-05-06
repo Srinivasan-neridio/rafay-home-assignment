857a4918327c19d8e00b530c8884516f682dabc9
Srinivasan-neridio <srinivasan@athinio.com>
2024-05-06 22:25:07 +0530

    Updated the Edit() and Delete() logic
    Added EditContact() and DeleteContact() new functions to perform accordingly

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ type Contact struct {
 	Email      string   `json:"mail"`
 	Company    string   `json:"company"`
 	Location   string   `json:"location"`
-	AddMobile  []string    `json:"extra_mobile"`
-	AddEmails  []string `json:"extra_mails"`
+	AddMobile  string    `json:"extra_mobile"`
+	AddEmails  string `json:"extra_mails"`
 	Phone
 }
 
@@ -70,12 +70,30 @@ func (member *Contact) Message(db *sql.DB, message string) error {
 	return nil
 }
 
-func (member *Contact) Edit(name, mail string, phone int) {
-	// update contact set first_name="Mr";
+func (member *Contact) Edit(db *sql.DB) error {
+	
+	fmt.Printf("\nIn Edit()")
+	
+	_, err := db.Exec("update contact set id = ?, first_name = ?, middle_name = ?, last_name = ?, mobile = ?, mail = ?, company = ?, location = ?, extra_mobile = ?, extra_mails = ?", member.ID, member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, member.AddMobile, member.AddEmails);
+	if err != nil {
+		fmt.Printf("\nError to update db: %s", err)
+		return err
+	}
+
+	return nil
 }
 
-func (member *Contact) Delete() {
-	// delete from contact where middle_name="Srinivasan";
+func (member *Contact) Delete(db *sql.DB) error {
+	
+	fmt.Printf("\nIn Delete()")
+	
+	_, err := db.Exec("delete from contact where id = ?", member.ID);
+	if err != nil {
+		fmt.Printf("\nError to update db: %s", err)
+		return err
+	}
+
+	return nil
 }
 
 func CreateContact(db *sql.DB) error {
@@ -96,10 +114,10 @@ func CreateContact(db *sql.DB) error {
 	member.Email = "srinivasan@athinio.com"
 	member.Company = "Neridio"
 	member.Location = "Bangalore"
-	member.AddMobile = append(member.AddMobile,"8220155210")
-	member.AddEmails = append(member.AddEmails,"mrsrinivasanofficial@gmail.com")
+	member.AddMobile = "8220155210"
+	member.AddEmails = "mrsrinivasanofficial@gmail.com"
 
-	_, err = db.Exec("insert into contact (first_name, middle_name, last_name, mobile, mail, company, location, extra_mobile, extra_mails) values(?, ?, ?, ?, ?, ?, ?, ?, ?);", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, member.AddMobile[0], member.AddEmails[0])
+	_, err = db.Exec("insert into contact (first_name, middle_name, last_name, mobile, mail, company, location, extra_mobile, extra_mails) values(?, ?, ?, ?, ?, ?, ?, ?, ?);", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, member.AddMobile, member.AddEmails)
 	if err != nil {
 		fmt.Printf("\nError to insert values: %s\n", err)
 		return err
@@ -111,10 +129,9 @@ func CreateContact(db *sql.DB) error {
 func CallContact(db *sql.DB, name string) error {
 	
 	var member Contact
-	var extraMobile, extraMail string
 
 	fmt.Printf("\nIn CallContact() %s", name)
-	err := db.QueryRow("select * from contact where id = ? or first_name = ? or middle_name = ? or last_name = ?;", name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &extraMobile, &extraMail)
+	err := db.QueryRow("select * from contact where id = ? or first_name = ? or middle_name = ? or last_name = ?;", name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &member.AddMobile, &member.AddEmails)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			fmt.Printf("\nNo rows found %s: %s", name, err)
@@ -124,7 +141,7 @@ func CallContact(db *sql.DB, name string) error {
 		}
 	}
 
-	fmt.Println("\n", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, extraMobile, extraMail)
+	fmt.Println("\n", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, member.AddMobile, member.AddEmails)
 	member.Call(db)
 
 	return nil
@@ -133,10 +150,9 @@ func CallContact(db *sql.DB, name string) error {
 func SearchContact(db *sql.DB, name string) error {
 	
 	var member Contact
-	var extraMobile, extraMail string
 
 	fmt.Printf("\nIn SearchContact() %s", name)
-	err := db.QueryRow("select * from contact where first_name = ? or middle_name = ? or last_name = ? or mobile = ? or mail = ? or company = ? or location = ? or extra_mobile = ? or extra_mails = ?;", name, name, name, name, name, name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &extraMobile, &extraMail)
+	err := db.QueryRow("select * from contact where first_name = ? or middle_name = ? or last_name = ? or mobile = ? or mail = ? or company = ? or location = ? or extra_mobile = ? or extra_mails = ?;", name, name, name, name, name, name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &member.AddMobile, &member.AddEmails)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			fmt.Printf("\nNo rows found %s: %s", name, err)
@@ -146,17 +162,16 @@ func SearchContact(db *sql.DB, name string) error {
 		}
 	}
 
-	fmt.Println("\n", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, extraMobile, extraMail)
+	fmt.Println("\n", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, member.AddMobile, member.AddEmails)
 	return nil
 }
 
 func SendMessage(db *sql.DB, name, message string) error {
 	
 	var member Contact
-	var extraMobile, extraMail string
 
 	fmt.Printf("\nIn SendMessage() %s %s", name, message)
-	err := db.QueryRow("select * from contact where id = ? or first_name = ? or middle_name = ? or last_name = ?;", name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &extraMobile, &extraMail)
+	err := db.QueryRow("select * from contact where id = ? or first_name = ? or middle_name = ? or last_name = ?;", name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &member.AddMobile, &member.AddEmails)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			fmt.Printf("\nNo rows found %s: %s", name, err)
@@ -166,7 +181,7 @@ func SendMessage(db *sql.DB, name, message string) error {
 		}
 	}
 
-	fmt.Println("\n", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, extraMobile, extraMail)
+	fmt.Println("\n", member.FirstName, member.MiddleName, member.LastName, member.Mobile, member.Email, member.Company, member.Location, member.AddMobile, member.AddEmails)
 	member.Message(db, message)
 
 	return nil
@@ -196,6 +211,67 @@ func SearchMessage(db *sql.DB, message string) error {
 	if err != nil {
 		fmt.Printf("\nError in row scan: %s", err)
 	}
+
+	return nil
+}
+
+func EditContact(db *sql.DB, name string) error {
+	
+	var oldmember, newmember Contact
+
+	fmt.Printf("\nIn EditContact() %s", name)
+	err := db.QueryRow("select * from contact where id = ? or first_name = ? or middle_name = ? or last_name = ?;", name, name, name, name).Scan(&oldmember.ID, &oldmember.FirstName, &oldmember.MiddleName, &oldmember.LastName, &oldmember.Mobile, &oldmember.Email, &oldmember.Company, &oldmember.Location, &oldmember.AddMobile, &oldmember.AddEmails)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Printf("\nNo rows found %s: %s", name, err)
+		} else {
+			fmt.Printf("\nError to query %s: %s", name, err)
+			return err
+		}
+	}
+
+	newmember.ID = oldmember.ID
+	fmt.Printf("\nExisting first name: %s, Enter the new one: ", oldmember.FirstName)
+	fmt.Scan(&newmember.FirstName)
+	fmt.Printf("\nExisting middle name: %s, Enter the new one: ", oldmember.MiddleName)
+	fmt.Scan(&newmember.MiddleName)
+	fmt.Printf("\nExisting last name: %s, Enter the new one: ", oldmember.LastName)
+	fmt.Scan(&newmember.LastName)
+	fmt.Printf("\nExisting mobile number: %s, Enter the new one: ", oldmember.Mobile)
+	fmt.Scan(&newmember.Mobile)
+	fmt.Printf("\nExisting email name: %s, Enter the new one: ", oldmember.Email)
+	fmt.Scan(&newmember.Email)
+	fmt.Printf("\nExisting company name: %s, Enter the new one: ", oldmember.Company)
+	fmt.Scan(&newmember.Company)
+	fmt.Printf("\nExisting location name: %s, Enter the new one: ", oldmember.Location)
+	fmt.Scan(&newmember.Location)
+	fmt.Printf("\nExisting extra mobile name: %s, Enter the new one: ", oldmember.AddMobile)
+	fmt.Scan(&newmember.AddMobile)
+	fmt.Printf("\nExisting extra email: %s, Enter the new one: ", oldmember.AddEmails)
+	fmt.Scan(&newmember.AddEmails)
+	
+	newmember.Edit(db)
+
+	return nil
+}
+
+func DeleteContact(db *sql.DB, name string) error {
+
+	var member Contact
+
+	fmt.Printf("\nIn DeleteContact() %s", name)
+	err := db.QueryRow("select * from contact where id = ? or first_name = ? or middle_name = ? or last_name = ?;", name, name, name, name).Scan(&member.ID, &member.FirstName, &member.MiddleName, &member.LastName, &member.Mobile, &member.Email, &member.Company, &member.Location, &member.AddMobile, &member.AddEmails)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Printf("\nNo rows found %s: %s", name, err)
+		} else {
+			fmt.Printf("\nError to query %s: %s", name, err)
+			return err
+		}
+	}
+
+	fmt.Printf("\nDo you want to delete this %s %s %s contact ?", member.FirstName, member.MiddleName, member.LastName)
+	member.Delete(db)
 
 	return nil
 }
@@ -270,8 +346,9 @@ func main() {
 	CallContact(db, "S")
 	SearchContact(db, "Srinivasan")
 	SendMessage(db, "Srinivasan", "Hey! what's up ?")
-	SearchMessage(db, "Hey! what's up ?")
-	// GetCallHistory(db)
+	// SearchMessage(db, "Hey! what's up ?")
+	EditContact(db, "Srinivasan")
+	DeleteContact(db, "Devil")
 
 	return
 }
